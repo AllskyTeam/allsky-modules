e44ea61406821a4c408d0fb850f03641eee1e109
alex-developer <alex.greenland@gmail.com>
2022-09-27 19:50:26 +0100

    Add avanced mode to cloud module

--- allsky_cloud/allsky_cloud.py
+++ allsky_cloud/allsky_cloud.py
@@ -24,7 +24,15 @@ metaData = {
     "arguments":{
         "i2caddress": "",
         "clearbelow": -10,
-        "cloudyabove": 5
+        "cloudyabove": 5,
+        "advanced": "false",
+        "k1": 0,
+        "k2": 0,
+        "k3": 0,
+        "k4": 0,
+        "k5": 0,
+        "k6": 0,
+        "k7": 0
     },
     "argumentdetails": {                   
         "i2caddress": {
@@ -56,7 +64,93 @@ metaData = {
                 "max": 100,
                 "step": 1
             }          
-        }
+        },
+        "advanced" : {
+            "required": "false",
+            "description": "Use advanced mode",
+            "help": "Provides a polynomial adjustment for the sky temperature",
+            "tab": "Advanced",
+            "type": {
+                "fieldtype": "checkbox"
+            }          
+        },
+        "k1" : {
+            "required": "true",
+            "description": "k1",
+            "tab": "Advanced",            
+            "type": {
+                "fieldtype": "spinner",
+                "min": 0,
+                "max": 500,
+                "step": 1
+            }          
+        },
+        "k2" : {
+            "required": "true",
+            "description": "k2",
+            "tab": "Advanced",            
+            "type": {
+                "fieldtype": "spinner",
+                "min": 0,
+                "max": 500,
+                "step": 1
+            }          
+        },
+        "k3" : {
+            "required": "true",
+            "description": "k3",
+            "tab": "Advanced",            
+            "type": {
+                "fieldtype": "spinner",
+                "min": 0,
+                "max": 500,
+                "step": 1
+            }          
+        },
+        "k4" : {
+            "required": "true",
+            "description": "k4",
+            "tab": "Advanced",            
+            "type": {
+                "fieldtype": "spinner",
+                "min": 0,
+                "max": 500,
+                "step": 1
+            }          
+        },
+        "k5" : {
+            "required": "true",
+            "description": "k5",
+            "tab": "Advanced",            
+            "type": {
+                "fieldtype": "spinner",
+                "min": 0,
+                "max": 500,
+                "step": 1
+            }          
+        },
+        "k6" : {
+            "required": "true",
+            "description": "k6",
+            "tab": "Advanced",            
+            "type": {
+                "fieldtype": "spinner",
+                "min": 0,
+                "max": 500,
+                "step": 1
+            }          
+        },
+        "k7" : {
+            "required": "true",
+            "description": "k7",
+            "tab": "Advanced",            
+            "type": {
+                "fieldtype": "spinner",
+                "min": 0,
+                "max": 500,
+                "step": 1
+            }          
+        }        
     },
     "enabled": "false"            
 }
@@ -68,23 +162,53 @@ def getsign(d):
         return 0.0
     return 1.0
 
+def calculateSkyStateAdvanced(skyambient, skyobject, clearbelow, cloudyabove, params):
+    k1 = int(params["k1"])
+    k2 = int(params["k2"])
+    k3 = int(params["k3"])
+    k4 = int(params["k4"])
+    k5 = int(params["k5"])
+    k6 = int(params["k6"])
+    k7 = int(params["k7"])
+
+    if abs((k2 / 10.0 - skyambient)) < 1:
+        t67 = getsign(k6) * getsign(skyambient - k2 / 10.) * abs((k2 / 10. - skyambient))
+    else:
+        t67 = k6 / 10. * getsign(skyambient - k2 / 10.) * (math.log(abs((k2 / 10. - skyambient))) / math.log(10) + k7 / 100)
+
+    td = (k1 / 100.) * (skyambient - k2 / 10.) + (k3 / 100.) * pow((math.exp(k4 / 1000. * skyambient)), (k5 / 100.)) + t67
+
+    tsky = skyobject - td
+    if tsky < clearbelow:
+        tsky = clearbelow
+    elif tsky > cloudyabove:
+        tsky = cloudyabove
+    cloudcover = ((tsky - clearbelow) * 100.) / (cloudyabove - clearbelow)
+    cloudcoverPercentage = "{}%".format(cloudcover)
+    cloudcover, percent = calculateSkyState(skyambient, skyobject, clearbelow, cloudyabove)
+    return cloudcover, cloudcoverPercentage
+
+
 
 def calculateSkyState(skyambient, skyobject, clearbelow, cloudyabove):
     cloudCover = 'Partial'
 
-    if skyambient <= clearbelow:
+    if skyobject <= clearbelow:
         cloudCover = 'Clear'
 
-    if skyambient >= cloudyabove:
+    if skyobject >= cloudyabove:
         cloudCover = 'Cloudy'
 
-    return cloudCover
+    return cloudCover, "N/A"
 
 
 def cloud(params):
     i2caddress = params["i2caddress"]
     clearbelow = int(params["clearbelow"])
     cloudyabove = int(params["cloudyabove"])
+
+    advanced = params["advanced"]
+
 
     if i2caddress != "":
         try:
@@ -102,13 +226,17 @@ def cloud(params):
     skyambient = mlx.ambient_temperature
     skyobject = mlx.object_temperature
 
-    cloudCover = calculateSkyState(skyambient, skyobject, clearbelow, cloudyabove)
+    if advanced:
+        cloudCover, percentage = calculateSkyStateAdvanced(skyambient, skyobject, clearbelow, cloudyabove, params)
+    else:
+        cloudCover, percentage = calculateSkyState(skyambient, skyobject, clearbelow, cloudyabove)
 
     os.environ["AS_CLOUDAMBIENT"] = str(skyambient)
     os.environ["AS_CLOUDSKY"] = str(skyobject)
     os.environ["AS_CLOUDCOVER"] = cloudCover
+    os.environ["AS_CLOUDCOVERPERCENT"] = percentage
 
-    result = "Cloud state - {0}. Sky Temp {1}, Ambient {2}".format(cloudCover, skyobject, skyambient)
+    result = "Cloud state - {0} {1}. Sky Temp {2}, Ambient {3}".format(cloudCover, percentage, skyobject, skyambient)
     s.log(1, "INFO: {}".format(result))
 
     return result   
