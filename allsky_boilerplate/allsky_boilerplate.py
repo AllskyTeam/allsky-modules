36b52fb218c8cd986c6829346aeac8e374c0c47c
Alex Greenland <alex.greenland@gmail.com>
2022-10-04 20:42:06 +0100

    Dev

--- allsky_boilerplate/allsky_boilerplate.py
+++ allsky_boilerplate/allsky_boilerplate.py
@@ -95,7 +95,13 @@ metaData = {
             "type": {
                 "fieldtype": "roi"
             }            
-        }                                                        
+        },
+        "textfield1": {
+            "required": "true",
+            "description": "Text Field1",
+            "help": "Example help for the text field in a new tab",
+            "tab": "Another Tab"
+        }                                                              
     },
     "enabled": "false"            
 }
@@ -118,14 +124,53 @@ def boilerplate(params, event):
 
     - They Must be fast if run during the day or night events. DO NOT do any long processing in these
       events as it will affect the capture process
-    - Consider carefully which events the module should run in. DO NOT just addevery event to the module
+    - Consider carefully which events the module should run in. DO NOT just add every event to the module
       as this will confuse the user
     - Provide a requirements.txt file with any Python dependencies. The generic module installer will
       detect this file and install the dependencies
     - Make use if the functions provided in the shared library rather that writing you own. See the main
       documentation for details of the functions available
+
+    Values available from the shared module
+
+    s.args - The arguments passed to the module processor (not normally needed)
+    s.LOGLEVEL - The allsky debug level (not normally ndded)
+    s.CURRENTIMAGEPATH - The full path to the current image (not normally needed, not available in periodic)
+    s.TOD - The time of day (Not available in periodic)
+    s.fullFilename - The final image filename i.e. image.jpg (not normally needed, not available in periodic)
+    s.image - a numpy array of the current image - Use this for any processing
+
+    Methods available in the shared module
+
+    s.log(level, message) - Logs an entry to the allsky log file if the debug level is above 'level'. Errors are always logged
+    s.getEnvironmentVariable(name, fatal=False, error='') - Gets an environment variable, can terminate if ndded
+    s.var_dump(variable) - Pretty dump of a variable
+    s.getSetting(settingName) - Gets a setting from the camera settings file
+    s.writeDebugImage(module, fileName, image) - Writes a debug image    
+    s.startModuleDebug(module) - Creates the debug directories for a module
+    s.convertPath(path) - Replaces allsky variables in a string
+    s.checkAndCreatePath(filePath) - Creates a path for a file
+    s.checkAndCreateDirectory(filePath) - Creates a directory
+    s.raining() - Only available if the rain module is being used - Returns flags to indicate the rain state
+    s.convertLatLon(input) - Converts 52.2N to 52.2 i.e converts to decimal 
+    s.setLastRun(module) - Sets the last run time for a module
+    s.shouldRun(module, period) - Determines if a module should run based on the period
+    s.dbAdd(key, value) - Adds the key/value pair to the internal database
+    s.dbUpdate(key, value) - Updates the key/value pair to the internal database
+    s.isFileWriteable(fileName) - Determines of the file is writeable
+    s.isFileReadable - Determines if the file is readable
     '''
 
     result = ""
+
+    if s.TOD == "day":
+        result = "It's daytime"
+        s.log(1,f"INFO: {result}")
+    elif s.TOD == "night:
+        result = "It's nighttime"
+        s.log(1,f"INFO: {result}")
+    else:
+        result "I don't know if its day or night!"
+        s.log(0,f"ERROR: {result}")
 
     return result
