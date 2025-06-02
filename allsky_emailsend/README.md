f4c271c2f34830523d594bbc4024eb8b1dd528b0
Kentner Cottingham <kentner.cottingham@dxc.com>
2025-06-03 07:09:28 +1200

    move files to backup

--- allsky_emailsend/README.md
+++ allsky_emailsend/README.md
@@ -7,41 +7,39 @@
 | **Runs In**  | Night to Day Transition<br>Periodic (only for testing)   |
 
 
-This module allows for automatically emailing nightly startrails, keogram, and timelapse images from a Gmail (or SMTP) account each morning.  Additionally it can create and send a composite startrails+keogram image which is a compressed keogram displayed below the startrails.
-
-Timelapse can be sent in a separate email from the other images if required or desired (eg. due to file size).
+This module allows for automatically emailing nightly startrails, keogram, and timelapse images from a Gmail (or SMTP) account each morning.  Additionally it can create and send a composite startrails+keogram image which is a compressed keogram displayed below the startrails.  The timelapse can be sent in a separate email from the other images if required or desired (eg. due to file size).
 
 **Prerequisites:**
- - A Google Account with MFA enabled and an app password.  OR an SMTP email server/service
-
- - For Gmail, see [Sign in with app passwords](https://support.google.com/accounts/answer/185833?hl=en) for details on how to setup an App Password.  _eg a 16-digit passcode used to give the script permission to access your Google Account to send email._
+ - For Gmail: An account with MFA enabled and an app password. See [Sign in with app passwords](https://support.google.com/accounts/answer/185833?hl=en) for details on how to setup an App Password.  _eg a 16-digit passcode used to give the script permission to access your Google Account to send email._
+ - Or annother SMTP email server/service
 
 ### Settings:
 | Daily Notification Setup          |||
 | ------------                      | ------------ |------------ |
-| To:                               | The address(es) to receive the email notification.  |Required|
-| Subject:                          | Configurable text for the email subject line |Optional|
-| Append date to Subject line       | Select to append Date Stamp to subject line text |Optional|
-| Email Message Text:               | Configurable text for the message body.<br>The filenames of any attached files will be appended to this text.|Optional|
-| Attach Images                     | Startrails / Keogram / Startrails and Keogram<br>Startrails Keogram Composite |Optional|
+| To or BCC?                        |Select how emails wil be delivered<br>TO or BCC|Required|
+| Recipients                        | The address(es) to receive the email notifications.  |Required|
+| Subject                           | Text for email subject line |Optional|
+| Append date to Subject            | Select to append Date Stamp to subject line text |Optional|
+| Email Message Text                | Enterable plain text for the message body.<br>Filenames of any attached files will be listed below this text.|Optional|
+| Attach Images                     | Startrails / Keogram / Startrails and Keogram<br>Startrails Keogram Composite / None |Optional|
 | Attach Timelapse                  | No / Yes / Yes - in separate email |Optional|
 ||||
 | **Composite Image Setup**|||
 | Padding                           | How much space to add between Startrails and Keogram <br> Default: 50 (0 - 1000)   | Required |
-| Height for Keogram                | Keogram is resized to this height x Startrails width <br> Default: 400 (20 - 2000) |Required  |
+| Keogram Height                    | Keogram is resized to [this height] x [Startrails width] <br> Default: 400 (20 - 2000) |Required  |
 | **Sender SMTP Setup**|||
-| Sender email address              | Sending account email address      | Required |
-| Password / <br>Google App Password|16-digit passcode from Google security settings<br> (or SMTP Password for other services)|Required|
-| SMTP server address               | Preset for Gmail|Required|
-| SMTP server port                  | Preset for Gmail|Required|
+| Sender email address              | Sender account email address      | Required |
+| Password / <br>Google App Password| 16-digit passcode from Google security settings<br>(or SMTP Password for other services)|Required|
+| SMTP server address               | Server address<br> Default: smtp.gmail.com|Required|
+| SMTP server port                  | Server port<br> Default: 587|Required|
+| Max attachments size              | Maximum allowed size of all attachments to an email in MB.<br> Default: 25|Required|
 
 ----------------------
 
 ### Notes:
 
- - Gmail has a maximum attachment limit of 25MB.  If your files exceed that limit then not all files will be attached.  You may have to modify timelapse creation options if that file is too large and you wish to send it via email.
+ - Gmail has a maximum attachment limit of 25MB.  If your files exceed the maximum allowed attachment size limit, then not all files will be attached.  You may have to modify timelapse creation options if that file is too large and you wish to send it via email.
 
- - This module can take a minute or two to complete depending on your Pi's internet connection and the size of the files to be attached.  Therefore it may make sense for this to be the last madule to run in your 'selected modules'
-
+ - This module can take a minute or more to complete depending on your Pi, internet connection, size of the files to be attached, etc.  Therefore it may make sense for this to be the last madule to run in your 'selected modules'
 
  - _Special Thanks to the prior work by Alex Greenland_, I referenced your disordsend module to get myself started with this one!
---
+++ allsky_emailsend/allsky_emailsend - Copy.py
@@ -0,0 +1,361 @@
+import allsky_shared as s
+import smtplib
+import os
+import datetime
+from email.message import EmailMessage
+import mimetypes
+import io
+from PIL import Image, ImageOps
+
+metaData = {
+    "name": "Email images via SMTP or Gmail",
+    "description": "Emails nightly images to email recipients",
+    "version": "v1.0",
+    "pythonversion": "3.9.0",
+    "module": "allsky_emailsend",    
+    "events": [
+        "nightday",
+        "periodic"
+    ],
+    "experimental": "false",  
+    "arguments": {
+        "address_as": "To",
+        "recipient_email": "",
+        "email_subject_text": "Last night's Allsky images",
+        "email_subject_date": "Yes",
+        "message_body": "Attached are last night's Allsky camera images.",
+        "image_selection":"Startrails Only",
+        "timelapse": "No",
+        "composite_padding": "50",
+        "composite_keogram_height": "400",
+        "sender_email_address": "", 
+        "sender_email_password": "",
+        "smtp_server": "smtp.gmail.com",
+        "smtp_port": "587",
+        "max_attachment_size_mb": "25"
+    },
+    "argumentdetails": {        
+        "address_as": {
+            "required": "false",
+            "description": "To or BCC",
+            "help": "",
+            "tab": "Daily Notification Setup",
+            "type": {
+                "fieldtype": "select",
+                "values": "To,BCC"
+            }              
+        },         
+        "recipient_email": {
+            "required": "true",
+            "description": "Recipients",
+            "help": "Separate with a comma if more than one.  eg. AAA@mail.com,BBB@mail.com",
+            "tab": "Daily Notification Setup"             
+        },
+        "email_subject_text": {
+            "required": "true",
+            "description": "Subject",
+            "help": "No characters like backslash or quote marks",
+            "tab": "Daily Notification Setup"             
+        },
+        "email_subject_date": {
+            "required": "false",
+            "description": "Append date to Subject",
+            "help": "eg: Last night's Allsky images - 20250401",
+            "tab": "Daily Notification Setup",
+            "type": {
+                "fieldtype": "select",
+                "values": "No,Yes"
+            }              
+        },
+        "message_body": {
+            "required": "true",
+            "description": "Email Message Text",
+            "help": "Any message body text you want to include. No characters like backslash or quote marks. File names are appended below this text.",
+            "tab": "Daily Notification Setup"             
+        },
+        "image_selection": {
+            "required": "false",
+            "description": "Attach Images",
+            "help": "Will only send if Allsky is configured to generate startrails and/or keogram.",
+            "tab": "Daily Notification Setup",
+            "type": {
+                "fieldtype": "select",
+                "values": "Startrails Only,Keogram Only,Startrails and Keogram,Startrails Keogram Composite,None"
+            }
+        },         
+        "timelapse": {
+            "required": "false",
+            "description": "Attach Timelapse",
+            "help": "",
+            "tab": "Daily Notification Setup",
+            "type": {
+                "fieldtype": "select",
+                "values": "No,Yes,Yes - in separate email"
+            }
+        },
+        "composite_padding": {
+            "required": "false",
+            "description": "Padding",
+            "help": "Space to add between Startrails and Keogram",
+            "tab": "Composite Image Setup",
+            "type": {
+                "fieldtype": "spinner",
+                "min": 0,
+                "max": 500,
+                "step": 20
+            }    
+        }, 
+        "composite_keogram_height": {
+            "required": "false",
+            "description": "Keogram Height",
+            "help": "Keogram is resized to [this height] x [Startrails width]",
+            "tab": "Composite Image Setup",
+            "type": {
+                "fieldtype": "spinner",
+                "min": 20,
+                "max": 2000,
+                "step": 20
+            }  
+        },
+        "sender_email_address": {
+            "required": "true",
+            "description": "Sender email address",
+            "help": "",
+            "tab": "Sender SMTP Setup"   
+        },        
+        "sender_email_password": {
+            "required": "true",        
+            "description": "Password / Google App Password",
+            "help": "For Gmail: get this from your google account's security settings, it is NOT your gmail login password.",
+            "tab": "Sender SMTP Setup"
+        },
+        "smtp_server": {
+            "required": "true",
+            "description": "SMTP server address",
+            "help": "Only change if not using Gmail",
+            "tab": "Sender SMTP Setup"          
+        },
+        "smtp_port": {
+            "required": "true",
+            "description": "SMTP server port",
+            "help": "Only change if not using Gmail",
+            "tab": "Sender SMTP Setup"
+        },
+        "max_attachment_size_mb": {
+            "required": "true",
+            "description": "Max attachments size",
+            "help": "In MB the maximum allowed size of all attachments to an email.",
+            "tab": "Sender SMTP Setup",
+            "type": {
+                "fieldtype": "spinner",
+                "min": 0,
+                "max": 200,
+                "step": 1
+            }  
+        }        
+    },
+    "enabled": "false",
+    "changelog": {
+        "v1.0": [
+            {
+                "author": "Kentner Cottingham",
+                "authorurl": "https://github.com/NiteRide",
+                "changes": "Initial Release"
+            }
+        ]                                       
+    }              
+}
+
+# Function to validate files to attach
+def validate_files(the_file_paths, total_attachment_size, max_attachment_size, message_body, valid_file_paths):
+    result = ""
+    valid_file_paths.clear()
+    for file_path in the_file_paths:
+        if os.path.exists(file_path):
+            file_size = os.path.getsize(file_path)
+            file_size_mb = round(file_size / 1024 / 1024, 1)
+            total_attachment_size += file_size
+            if total_attachment_size > max_attachment_size:
+                message_body += f"\n{os.path.basename(file_path)} too large to attach: ({file_size_mb}mb)"
+                result += f"Error: Total attachment size exceeds 25MB limit. File not attached: {file_path}\n"
+            else:
+                message_body += f"\n{os.path.basename(file_path)}  ({file_size_mb}mb)"
+                valid_file_paths.append(file_path)
+        else:
+            result += f"Error: File does not exist: {file_path}\n"
+    return result, total_attachment_size, message_body
+
+# Function to create composite startrails-keogram image
+def create_combo_image(output_path, file_path_stars, file_path_keo, keo_height, img_padding):
+    result = ""
+    try:
+        with Image.open(file_path_stars) as stars_img:
+            stars_width, stars_height = stars_img.size
+            
+            with Image.open(file_path_keo) as keo_img:
+                keo_img = keo_img.resize((stars_width, keo_height))
+
+            ttl_height = stars_height + img_padding + keo_height
+
+            combined_img = Image.new("RGB", (stars_width, ttl_height), (0, 0, 0))
+            combined_img.paste(stars_img, (0, 0))
+            combined_img.paste(keo_img, (0, stars_height + img_padding))
+            combined_img.save(output_path)
+            result += f"Composite image saved to {output_path}\n"
+
+        return result
+    except Exception as e:
+        result += f"Error creating composite image: {e}\n"
+        return result
+
+# Function to attach valid files to the email
+def attach_files(the_email_msg, attach_files):
+    result = ""
+    for file_path in attach_files:
+        try:
+            mime_type, _ = mimetypes.guess_type(file_path)
+            maintype, subtype = mime_type.split('/')
+            with open(file_path, "rb") as f:
+                the_email_msg.add_attachment(f.read(), maintype=maintype, subtype=subtype, filename=os.path.basename(file_path))
+                result += f"Attached: {file_path}\n"
+        except Exception as e:
+            result += f"Error attaching file: {e}\n"
+    return result
+
+# Function to send email via Gmail SMTP / TLS
+def send_email_now(the_email_msg, smtp_server, smtp_port, sender_email_address, sender_email_password):
+    result = ""
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()  # Secure connection
+            server.login(sender_email_address, sender_email_password)
+            server.send_message(the_email_msg)
+        result = "\nEmail sent successfully"
+    except Exception as e:
+        result = f"\nError sending email: {e}"
+    return result
+
+# Main Module Function
+def emailsend(params, event):
+    # Gmail SMTP configuration and parameters
+    smtp_server = params['smtp_server']
+    smtp_port = params['smtp_port']
+    sender_email_address = params['sender_email_address']
+    sender_email_password = params['sender_email_password']
+    recipient_email = params['recipient_email']
+    email_subject_text = params['email_subject_text']
+    email_subject_date = params['email_subject_date']
+    message_body = params['message_body']
+    image_selection = params['image_selection']
+    composite_padding = params['composite_padding']
+    composite_keogram_height = params['composite_keogram_height']
+    timelapse = params['timelapse']
+    to_or_bcc = params['address_as']
+    max_attachment_size_mb = params['max_attachment_size_mb']
+
+    startrails = 'No'
+    keogram = 'No'
+    composite = 'No'
+
+    result = ""
+    send_email = False
+
+    # Get user's home directory
+    home_dir = os.path.expanduser("~")
+
+    # Get yesterday's date in YYYYMMDD format
+    yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y%m%d")
+    
+    # get image file extension from allsky user settings      
+    full_file_name = s.getSetting('filename')
+    _, file_ext = os.path.splitext(full_file_name)
+
+    # Initialize email message details
+    msg = EmailMessage()
+    msg["From"] = sender_email_address
+    match to_or_bcc:
+        case "To": msg["To"] = recipient_email
+        case "BCC": msg["BCC"] = recipient_email
+    if email_subject_date == "Yes":
+        msg["Subject"] = f"{email_subject_text} - {yesterday}"
+    else:
+        msg["Subject"] = email_subject_text
+    
+    match image_selection:
+        case "Startrails Only": startrails = "Yes"
+        case "Keogram Only": keogram ="Yes"
+        case "Startrails and Keogram": 
+            startrails ="Yes"
+            keogram ="Yes"
+        case "Startrails Keogram Composite": composite ="Yes"
+
+    # Initialize total attachment size (max is 25MB for gmail)
+    max_attachment_size = 25 * 1024 * 1024
+    total_attachment_size = 0
+    file_paths_images = []
+    file_paths_video = []
+    valid_file_paths = []  
+
+    file_path_stars = file_path = os.path.join(home_dir, f"allsky/images/{yesterday}/startrails/startrails-{yesterday}{file_ext}")
+    file_path_keo = os.path.join(home_dir, f"allsky/images/{yesterday}/keogram/keogram-{yesterday}{file_ext}")
+    file_path_timelapse = os.path.join(home_dir, f"allsky/images/{yesterday}/allsky-{yesterday}.mp4")
+    file_path_composite = os.path.join(home_dir, f"allsky/images/{yesterday}/startrails/startrails-keo-{yesterday}{file_ext}")
+
+    # Check user file selections
+    if composite =="Yes":
+            if os.path.exists(file_path_stars) and os.path.exists(file_path_keo):
+                # create composite
+                composite_img = create_combo_image(file_path_composite, file_path_stars, file_path_keo, composite_keogram_height, composite_padding)
+                # attach composite
+                if composite_img:
+                    file_paths_images.append(file_path_composite)
+                    result += "Attached composite image\n"
+                send_email = True
+
+    if startrails == "Yes":
+        file_paths_images.append(file_path_stars)
+        send_email = True
+        
+    if keogram == "Yes":
+        file_paths_images.append(file_path_keo)
+        send_email = True
+
+    if timelapse == "Yes":
+        file_paths_images.append(file_path_timelapse)
+        send_email = True
+
+    if send_email:
+        # Validate file paths and file size
+        validation_result, total_attachment_size, message_body = validate_files(file_paths_images, total_attachment_size, max_attachment_size, message_body, valid_file_paths)
+        result += validation_result
+        # Set the main body content details BEFORE attaching files
+        msg.set_content(message_body)
+        # Attach the valid files
+        result += attach_files(msg, valid_file_paths)
+        # Send the email
+        result += send_email_now(msg, smtp_server, smtp_port, sender_email_address, sender_email_password)
+
+    # If user wants timelapse sent separately
+    if timelapse == "Yes - in separate email":
+        file_path = os.path.join(home_dir, f"allsky/images/{yesterday}/allsky-{yesterday}.mp4")
+        file_paths_video.append(file_path)
+
+        # Reuse same base message body
+        message_body = params['message_body']
+        
+        msg_video = EmailMessage()
+        msg_video["From"] = sender_email_address
+        msg_video["To"] = recipient_email
+        msg_video["Subject"] = msg["Subject"]
+        
+        # Validate file paths and file size
+        validation_result, total_attachment_size, message_body = validate_files(file_paths_video, total_attachment_size, max_attachment_size, message_body, valid_file_paths)
+        result += validation_result
+        # Set main body content BEFORE attaching files
+        msg_video.set_content(message_body)
+        # Attach the valid files
+        result += attach_files(msg_video, valid_file_paths)
+        # Send the email
+        result += send_email_now(msg_video, smtp_server, smtp_port, sender_email_address, sender_email_password)    
+    
+    return result
