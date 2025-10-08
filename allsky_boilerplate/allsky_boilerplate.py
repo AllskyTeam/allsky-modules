43b092e29c2ff054cd498a68241085f920175f71
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-10-08 06:39:46 -0500

    Update allsky_boilerplate.py: Update name, description, and help

--- allsky_boilerplate/allsky_boilerplate.py
+++ allsky_boilerplate/allsky_boilerplate.py
@@ -2,18 +2,17 @@
 allsky_boilerplate.py
 
 Part of allsky postprocess.py modules.
-https://github.com/thomasjacquin/allsky
-
-
+https://github.com/AllskyTeam/allsky
 '''
+
 import allsky_shared as allsky_shared
 from allsky_base import ALLSKYMODULEBASE
 
 class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 
 	meta_data = {
-		"name": "All Sky Boilerplate",
-		"description": "Example module for AllSky module developers",
+		"name": "Boilerplate Module",
+		"description": "Example module for Allsky module developers",
 		"module": "allsky_boilerplate",
 		"version": "v1.0.3",
 		"centersettings": "false",
@@ -60,13 +59,13 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 			"textfield": {
 				"required": "false",
 				"description": "Text Field",
-				"help": "Example help for the text field",
+				"help": "Example help for the text field.",
 				"tab": "Field Types"
 			},  
 			"select" : {
 				"required": "false",
 				"description": "Select Field",
-				"help": "Example help for a select field",
+				"help": "Example help for a select field.",
 				"tab": "Field Types",
 				"type": {
 					"fieldtype": "select",
@@ -77,7 +76,7 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 				"required": "false",
 				"description": "Ajax Select",
 				"tab": "Field Types",
-				"help": "returns data from an ajax request",
+				"help": "returns data from an AJAX request.",
 				"type": {
 					"fieldtype": "ajaxselect",
 					"url": "includes/moduleutil.php?request=Onewire",
@@ -87,7 +86,7 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 			"checkbox" : {
 				"required": "false",
 				"description": "Checkbox Field",
-				"help": "Example help for the checkbox field",
+				"help": "Example help for the checkbox field.",
 				"tab": "Field Types",
 				"type": {
 					"fieldtype": "checkbox"
@@ -96,7 +95,7 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 			"number" : {
 				"required": "true",
 				"description": "Number Field",
-				"help": "Example help for the number field",
+				"help": "Example help for the number field.",
 				"tab": "Field Types",
 				"type": {
 					"fieldtype": "spinner",
@@ -108,7 +107,7 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 			"i2c": {
 				"required": "false",
 				"description": "I2C Address",
-				"help": "Example field to select an i2c address",
+				"help": "Example field to select an i2c address.",
 				"tab": "Field Types",           
 				"type": {
 					"fieldtype": "i2c"
@@ -117,7 +116,7 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 			"gpio": {
 				"required": "false",
 				"description": "GPIO Field",
-				"help": "Example help for the GPIO field",
+				"help": "Example help for the GPIO field.",
 				"tab": "Field Types 2",
 				"type": {
 					"fieldtype": "gpio"
@@ -126,7 +125,7 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 			"image" : {
 				"required": "false",
 				"description": "Image Field",
-				"help": "Example help for the image field",
+				"help": "Example help for the image field.",
 				"tab": "Field Types 2",            
 				"type": {
 					"fieldtype": "image"
@@ -135,7 +134,7 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 			"roi": {
 				"required": "false",
 				"description": "Region of Interest field",
-				"help": "Help for the region of interest field",
+				"help": "Example help for the region of interest field.",
 				"tab": "Field Types 2",            
 				"type": {
 					"fieldtype": "roi"
@@ -144,7 +143,7 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 			"variable": {
 				"required": "false",
 				"description": "Allsky Variable",
-				"help": "Allows and Allsky Variable to be selected",
+				"help": "Allows and Allsky variable to be selected.",
 				"tab": "Field Types 2",         
 				"type": {
 					"fieldtype": "variable",
@@ -153,7 +152,7 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 			},
 			"position": {
 				"description": "Position",
-				"help": "Allows lat, lon and altitude to be selected from a map",
+				"help": "Allows latitude, longitude, and altitude to be selected from a map.",
 				"tab": "Field Types 2",
 				"lat": {
 					"id": "observer_lat"
@@ -172,14 +171,14 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 				"required": "true",
 				"tab": "Field Types 2",          
 				"description": "Allows a url to be entered and then checked to see if it can be reached",
-				"help": "ASelect a url",
+				"help": "Select a url.",
 				"type": {
 					"fieldtype": "url"
 				}          
 			},
 			"host": {
 				"description": "Host",
-				"help": "Allows a host and port to be entered",
+				"help": "Allows a host and port to be entered.",
 				"tab": "Field Types 2",
 				"url": {
 					"id": "influxhost"
@@ -211,13 +210,13 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
    			"textfield1": {
 				"required": "true",
 				"description": "Text Field1",
-				"help": "This field is always shown as it has no filters",
+				"help": "This field is always shown as it has no filters.",
 				"tab": "Filters"
 			},
    			"textfield2": {
 				"required": "true",
 				"description": "Text Field 2",
-				"help": "This field is shown when value1 is selected in the select filter field",
+				"help": "This field is shown when value1 is selected in the select filter field.",
 				"tab": "Filters",
 				"filters": {
 					"filter": "filterselect",
@@ -230,7 +229,7 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 			"select2" : {
 				"required": "false",
 				"description": "Select Field",
-				"help": "This field is shown when value1 is selected in the select filter field",
+				"help": "This field is shown when value1 is selected in the select filter field.",
 				"tab": "Filters",
 				"type": {
 					"fieldtype": "select",
@@ -247,7 +246,7 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 			"gpio1": {
 				"required": "false",
 				"description": "GPIO Field",
-				"help": "This field is shown when value2 is selected in the select filter field",
+				"help": "This field is shown when value2 is selected in the select filter field.",
 				"tab": "Filters",
 				"type": {
 					"fieldtype": "gpio"
@@ -263,7 +262,7 @@ class ALLSKYBOILERPLATE(ALLSKYMODULEBASE):
 			"html": {
 				"tab": "Help",
     			"source": "local",
-                "html": "<h1>This help text is defined within the module</h1><blockquote>This help is hard coded into the modules config. This is ok for very short text but not good for longer. For longer text use the file option in the field</blockquote><p>Bacon ipsum dolor amet cow chislic strip steak pig pork belly chuck pork doner salami. Pig bresaola kielbasa rump. Jerky beef strip steak jowl beef ribs, kielbasa corned beef fatback cupim pork loin. Swine strip steak tongue turkey pig. Pork belly turducken boudin rump venison.</p><p>Burgdoggen beef pig ribeye, kielbasa biltong filet mignon shank turkey spare ribs ground round meatball sirloin picanha. Brisket t-bone doner, tri-tip jowl ham biltong kielbasa. Fatback chicken kevin tail prosciutto, frankfurter kielbasa capicola rump tri-tip andouille chislic swine pork loin filet mignon. T-bone ribeye spare ribs kevin, sausage turkey short loin short ribs filet mignon tri-tip bresaola. Beef ribs chislic pork, alcatra beef pig ball tip doner jowl shank kevin kielbasa meatloaf spare ribs. Ground round ham tail, flank pastrami kevin jerky.</p>",
+                "html": "<h1>This help text is defined within the module</h1><blockquote>This help is hard coded into the modules config. This is ok for very short text but not good for longer text. For longer text use the file option in the field</blockquote><p>Bacon ipsum dolor amet cow chislic strip steak pig pork belly chuck pork doner salami. Pig bresaola kielbasa rump. Jerky beef strip steak jowl beef ribs, kielbasa corned beef fatback cupim pork loin. Swine strip steak tongue turkey pig. Pork belly turducken boudin rump venison.</p><p>Burgdoggen beef pig ribeye, kielbasa biltong filet mignon shank turkey spare ribs ground round meatball sirloin picanha. Brisket t-bone doner, tri-tip jowl ham biltong kielbasa. Fatback chicken kevin tail prosciutto, frankfurter kielbasa capicola rump tri-tip andouille chislic swine pork loin filet mignon. T-bone ribeye spare ribs kevin, sausage turkey short loin short ribs filet mignon tri-tip bresaola. Beef ribs chislic pork, alcatra beef pig ball tip doner jowl shank kevin kielbasa meatloaf spare ribs. Ground round ham tail, flank pastrami kevin jerky.</p>",
 				"type": {
 					"fieldtype": "html"
 				}
